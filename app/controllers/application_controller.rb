62f7eb91d6069ebf57f883ebf5494814a2bf9a8a
keiadachi <2013edcckeiadachi@gmail.com>
2021-05-08 14:46:18 +0000

    guest/devise/view編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :name,
+      :postal_code,
+      :address,
+      :phone_number])
+  end
+#guest/deviesにカラム追加後、上記記入すればデータ保存がされる
+
+
+
 end
