5e4bfd5fe507b3c555707a8dff6e0778dbb480fe
keiadachi <2013edcckeiadachi@gmail.com>
2021-05-09 15:37:51 +0000

    mypage

--- app/controllers/guest/guests_controller.rb
+++ app/controllers/guest/guests_controller.rb
@@ -3,8 +3,28 @@ class Guest::GuestsController < ApplicationController
   end
 
   def edit
+    @guest = Guest.find(params[:id])
   end
 
   def update
+    @guest = current_guest
+      if @guest.update(guest_params)
+        redirect_to root_path
+      else
+        render 'edit'
+      end
   end
+
+  private
+  def guest_params
+    params.require(:guest).permit(:email, :guest_passward, :name, :postal_code, :address, :phone_number, :guest_status)
+  end
+
+  def ensure_correct_guest
+    @guest = Guest.find(params[:id])
+      if current_guest.id != @guest.id
+         redirect_to root_path
+      end
+  end
+
 end
