f81d499e991cebbb8a3fd3ad7eb098d053f19b76
keiadachi <2013edcckeiadachi@gmail.com>
2021-05-09 10:52:50 +0000

    gueststatus

--- app/controllers/admin/guests_controller.rb
+++ app/controllers/admin/guests_controller.rb
@@ -1,14 +1,22 @@
 class Admin::GuestsController < ApplicationController
   def index
+    @guests = Guest.all
   end
 
   def show
   end
 
   def edit
+    @guest = Guest.find(params[:id])
   end
 
   def update
+    @guest = Guest.find(params[:id])
+     if @guest.update(guest_params)
+       redirect_to admin_guests_path
+     else
+       render "edit"
+     end
   end
 
   def quit
@@ -16,4 +24,10 @@ class Admin::GuestsController < ApplicationController
 
   def out
   end
+
+  private
+  def guest_params
+    params.require(:guest).permit(:email, :guest_passward, :name, :postal_code, :address, :phone_number, :guest_status)
+  end
+
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -3,4 +3,12 @@ class Guest < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def active_for_authentication?
+    super && (self.guest_status == true)
+  end
+
+
+
 end
