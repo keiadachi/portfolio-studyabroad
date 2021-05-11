4244855138148a47d57eb51c6dc325ec44ce7031
keiadachi <2013edcckeiadachi@gmail.com>
2021-05-11 14:16:07 +0000

    admin/faq

--- app/controllers/admin/faqs_controller.rb
+++ app/controllers/admin/faqs_controller.rb
@@ -1,19 +1,41 @@
 class Admin::FaqsController < ApplicationController
   def index
+    @faqs = Faq.all
   end
 
   def new
+    @faq = Faq.new
   end
 
   def show
   end
 
   def edit
+    @faq = Faq.find(params[:id])
   end
 
   def create
+    @faq = Faq.new(faq_params)
+    if @faq.save
+      redirect_to admin_faqs_path
+    else
+      render :new
+    end
   end
 
   def update
+    @faq = Faq.find(params[:id])
+      if @faq.update(faq_params)
+        redirect_to admin_faqs_path(@faqs)
+      else
+        redirect_to edit_admin_faq_path(@faq)
+      end
   end
+
+
+  private
+  def faq_params
+    params.require(:faq).permit(:question, :answer)
+  end
+
 end
